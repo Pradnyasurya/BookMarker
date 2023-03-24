6b07b90aa5911d37fe4c4da336d7f7eb26a96d43
Pradnyasurya Kurane <kuranepm@gmail.com>
2023-03-24 22:56:30 +0530

    Created a basic app.

--- bookmarker/bookmarker/src/main/java/com/surya/bookmarker/BookmarkerApplication.java
+++ bookmarker/bookmarker/src/main/java/com/surya/bookmarker/BookmarkerApplication.java
@@ -1,12 +1,17 @@
 package com.surya.bookmarker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class BookmarkerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookmarkerApplication.class, args);
 	}
 
---
+++ bookmarker/bookmarker/src/main/java/com/surya/bookmarker/domain/BookmarkRepository.java
@@ -0,0 +1,8 @@
+package com.surya.bookmarker.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+}
